b87e3e0100a076d61a66fc09c1202b42f8462480
Shehan Atuk <shehanatukorala@Shehans-MacBook-Pro.local>
2019-01-30 12:32:56 -0500

    hardcode A1 finished

--- 60-256A1.c
+++ 60-256A1.c
@@ -2,94 +2,99 @@
 #include <unistd.h>
 #include <fcntl.h>
 void writeFunction();
+unsigned convertHeightToInt(char a[], unsigned b);
+unsigned convertWidthToInt(char a[], unsigned b);
+unsigned concatenate(unsigned x, unsigned y);
 void getDimens();
-	double fileSize1 = 0, fileSize2 = 0, fileSize3 = 0;
-	double fd1, fd2, fd3;
-	char height1[100], width1[100];
-	int height2, width2;
+	int fd1, fd2, fd3;
+	char file2Height[10], file2Width[100];
+	unsigned file2H, file2W;
+	char file1Height[10], file1Width[10];
+	unsigned file1H, file1W;
 	char buffer2[3];
+	unsigned count;
 int main(int argc, char *argv[]){
-	
-
-
-	//getDimens();
+	getDimens();
+	file2H = (int)convertHeightToInt(file2Height, file2H);
+	file2W = (int)convertWidthToInt(file2Width, file2W);
+	file1H = (int)convertHeightToInt(file1Height, file1H);
+	file1W = (int)convertWidthToInt(file1Width, file1W);
 	writeFunction();
-	// lseek(fd2, 0, SEEK_SET);
-	// lseek(fd3, fileSize2, SEEK_SET);
-	// read(fd2, buffer, fileSize2);
-	// write(fd3, &buffer, fileSize2);
-   
+	
 }
 void getDimens(){
-	fd1 = open("/Users/shehanatukorala/Desktop/Desktop/img1.ppm", O_RDONLY);
-	fd2 = open("/Users/shehanatukorala/Desktop/Desktop/img2.ppm", O_RDONLY);
-	fd3 = open("/Users/shehanatukorala/Desktop/Desktop/img3.ppm", O_CREAT | O_WRONLY , 0755);
-	lseek(fd2, 0, SEEK_SET);
-	read(fd1, &height1, 60);
-	lseek(fd1, 0, SEEK_SET);
-	lseek(fd3, 0, SEEK_SET);
-	write(fd3, &height1, 60);
-	write(fd3, "\n", 1);
-	lseek(fd3, 60, SEEK_SET);
-	lseek(fd2, 0, SEEK_SET);
-	read(fd2, &height1, 60);
-	write(fd3, &height1, 60);
-while(read(fd2, &buffer2, 1) > 0){
-		fileSize2++;//fd1 file size
-	}
-	printf("%d\n", fileSize2);
-	printf("%s\n", height1);
-	printf("%s\n", width1);
+	fd1 = open("../img1.ppm", O_RDONLY);
+	fd2 = open("../img2.ppm", O_RDONLY);
+	lseek(fd2, 48, SEEK_SET);
+	read(fd2, &file2Width, 4);
+	lseek(fd2, 53, SEEK_SET);
+	read(fd2, &file2Height, 3); 
+	lseek(fd1, 48, SEEK_SET);
+	read(fd1, &file1Width, 4);
+	lseek(fd1, 53, SEEK_SET);
+	read(fd1, &file1Height, 3);
 
-	height2 = (int)height1;
-	width2 = (int) width1;
 }
 void writeFunction(){
-	fd1 = open("/Users/shehanatukorala/Desktop/Desktop/img1.ppm", O_RDONLY);
-	fd2 = open("/Users/shehanatukorala/Desktop/Desktop/img2.ppm", O_RDONLY);
-	fd3 = open("/Users/shehanatukorala/Desktop/Desktop/img3.ppm", O_CREAT | O_WRONLY | O_TRUNC , 0755);
-	while(read(fd1, &buffer2, 3) > 0){
-		fileSize1++;//fd1 file size
-	}
-	while(read(fd2, &buffer2, 3) > 0){
-		fileSize2++;
-	}
-	printf("fileSize1: %f, fileSize2: %f", fileSize1, fileSize2);
-//img1 dimens: 1080 792 = 855360
-//img2 dimens: 1001 667 = 667667
+	fd1 = open("../stop11.ppm", O_RDONLY);
+	fd2 = open("../stop12.ppm", O_RDONLY);
+	fd3 = open("../stop13.ppm", O_CREAT | O_WRONLY , 0777);
 	char buffer[3];
-	lseek(fd1, 0, SEEK_SET);
-	lseek(fd3, -fileSize2, SEEK_END);
-	for(int i = 0;  i < fileSize1; i++){
-		read(fd1, buffer, 3);
-		write(fd3, &buffer, 3);
-	}
-	double count = 2.5;
-	double counter = (fileSize1*2.5);
-	lseek(fd3, -counter, SEEK_END);
-	lseek(fd2, 0, SEEK_SET);
-	for(int j = 0 ; j < 5; j++){
-		for(int i =0; i < 1001; i++){
-				read(fd2, buffer, 3);
-				write(fd3, &buffer, 3);
+	// lseek(fd1, 0, SEEK_SET);
+	// lseek(fd3, 0, SEEK_SET);
+	
+	for(int i = 0;  i < file1W; i++){
+		for(int j = 0 ; j < file1W; j++){
+			read(fd1, buffer, 3);
+			write(fd3, &buffer, 3);
 		}
-		lseek(fd3, -(counter+((1080-1001)*3)), SEEK_END);
-	}		
-	
-	
-	//	counter+= 792;
-//	lseek(fd3, counter, SEEK_SET);
-	//	
-	//}
-	
-	
-	// for(int i = 0; i < 1001; i++){
-	// 	read(fd2, buffer, 3);
-	// 	write(fd3, &buffer, 3);
-	// }
+	}
+	int counter = (int)(file1W - file2W);
+//	printf("%d, %d\n", (int)counter, (int)file1H*3);
+//	printf("file1H: %d file1W: %d\n", (int)file1H, (int)file1W);
+//	printf("file2H: %d file2W: %d\n", (int)file2H, (int)file2W);
 
+	lseek(fd3, (counter+ 218), SEEK_SET);
+	printf("%d\n",counter);
+	lseek(fd2, 0, SEEK_SET);
+	int count = 0;
+	for(int j = 0; j < (int)file2W/1.5 ; j++){
+		for(int i = 0 ; i < (int)(file2W); i++){
+			read(fd2, buffer, 3);
+			write(fd3, &buffer, 3);
+		}
+			lseek(fd3, ((int)counter+158),SEEK_CUR);
+	}
 
 	close(fd1);
     close(fd2);
     close(fd3);
 }
+
+unsigned concatenate(unsigned x, unsigned y){
+	unsigned pow = 10;
+//	printf("%d, %d\n",x,y);
+	while(y >= pow){
+		pow *= 10;
+		printf("%d\n", y);
+	}
+	return x * pow + y;
+}
+unsigned convertHeightToInt(char a[],  unsigned b){
+	unsigned count;
+	b = (int)a[0]-'0';
+	for(int i = 1 ; i< 3; i++){
+		b = concatenate(b, (int)a[i]-'0');
+	}
+	return b;
+}
+unsigned convertWidthToInt(char a[],  unsigned b){
+	unsigned count;
+	b = (int)a[0]-'0';
+	for(int i = 1 ; i< 4; i++){
+		b = concatenate(b, (int)a[i]-'0');
+	}
+	return b;
+}
+
+
